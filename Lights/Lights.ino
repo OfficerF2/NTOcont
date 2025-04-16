f886a0faad2ac63ee2a8fc1425674df4b7f76964
Gosha <GeorgiiVarfg@gmail.com>
2025-04-16 18:03:58 +0700

    Svetofor First

--- Lights/Lights.ino
+++ Lights/Lights.ino
@@ -8,152 +8,16 @@ BH1750 LightSensor_1; // экземпляры пяти датчиков осве
 BH1750 LightSensor_2;
 BH1750 LightSensor_3;
 BH1750 LightSensor_4;
-PCA9634 testModule(0x08);
-PCA9634 testModule1(0x0C);
-PCA9634 testModule2(0x18);
-PCA9634 testModule3(0x1C);
+
+bool light1 = false;
+bool light2 = false;
+bool light3 = false;
+bool light4 = false;
 
 #define I2C_HUB_ADDR        0x70
 #define EN_MASK             0x08
 #define DEF_CHANNEL         0x00
 #define MAX_CHANNEL         0x08
-/*
-  I2C порт 0x07 - выводы GP16 (SDA), GP17 (SCL)
-  I2C порт 0x06 - выводы GP4 (SDA), GP13 (SCL)
-  I2C порт 0x05 - выводы GP14 (SDA), GP15 (SCL)
-  I2C порт 0x04 - выводы GP5 (SDA), GP23 (SCL)
-  I2C порт 0x03 - выводы GP18 (SDA), GP19 (SCL)
-*/
-void setup() {
-  // Инициализация последовательного порта
-  Serial.begin(115200);
-  testModule.begin();
-  testModule1.begin();
-  testModule2.begin();
-  testModule3.begin();
-  Wire.begin();
-  delay(1000);
-
-  // Инициализация датчика #1
-  setBusChannel(0x03); // функция смены I2C-порта
-  LightSensor_1.begin();
-
-  // Инициализация датчика #2
-  setBusChannel(0x04); // функция смены I2C-порта
-  LightSensor_2.begin();
-
-  // Инициализация датчика #3
-  setBusChannel(0x05); // функция смены I2C-порта
-  LightSensor_3.begin();
-
-  setBusChannel(0x07); // функция смены I2C-порта
-  LightSensor_4.begin();
-
-    for (int channel = 0; channel < testModule.channelCount(); channel++)
-  {
-    testModule.setLedDriverMode(channel, PCA9634_LEDON);
-    Serial.println(channel);
-    delay(500);
-    testModule.setLedDriverMode(channel, PCA9634_LEDOFF);
-    delay(500);
-  }
-  for (int channel = 0; channel < testModule.channelCount(); channel++)
-  {
-    testModule.setLedDriverMode(channel, PCA9634_LEDPWM); // установка режима ШИМ (0-255)
-  }
-
-    for (int channel = 0; channel < testModule1.channelCount(); channel++)
-  {
-    testModule1.setLedDriverMode(channel, PCA9634_LEDON);
-    Serial.println(channel);
-    delay(500);
-    testModule1.setLedDriverMode(channel, PCA9634_LEDOFF);
-    delay(500);
-  }
-  for (int channel = 0; channel < testModule1.channelCount(); channel++)
-  {
-    testModule1.setLedDriverMode(channel, PCA9634_LEDPWM); // установка режима ШИМ (0-255)
-  }
-    for (int channel = 0; channel < testModule2.channelCount(); channel++)
-  {
-    testModule2.setLedDriverMode(channel, PCA9634_LEDON);
-    Serial.println(channel);
-    delay(500);
-    testModule2.setLedDriverMode(channel, PCA9634_LEDOFF);
-    delay(500);
-  }
-  for (int channel = 0; channel < testModule2.channelCount(); channel++)
-  {
-    testModule2.setLedDriverMode(channel, PCA9634_LEDPWM); // установка режима ШИМ (0-255)
-  }
-    for (int channel = 0; channel < testModule3.channelCount(); channel++)
-  {
-    testModule3.setLedDriverMode(channel, PCA9634_LEDON);
-    Serial.println(channel);
-    delay(500);
-    testModule3.setLedDriverMode(channel, PCA9634_LEDOFF);
-    delay(500);
-  }
-  for (int channel = 0; channel < testModule3.channelCount(); channel++)
-  {
-    testModule3.setLedDriverMode(channel, PCA9634_LEDPWM); // установка режима ШИМ (0-255)
-  }
-}
-void loop() {
-  // Считывание датчиков
-  setBusChannel(0x03);
-  float light1 = LightSensor_1.readLightLevel();
-  setBusChannel(0x04);
-  float light2 = LightSensor_2.readLightLevel();
-  setBusChannel(0x05);
-  float light3 = LightSensor_3.readLightLevel();
-  setBusChannel(0x07);
-  float light4 = LightSensor_4.readLightLevel();
-
-
-  if (light1 < 250){
-  testModule.write1(3, 0x90);
-  testModule.write1(2, 0x90);
-  testModule.write1(5, 0x00);
-  }else{
-  testModule.write1(3, 0x00);
-  testModule.write1(2, 0x00);
-  testModule.write1(5, 0x00);
-  }
-  if (light2 < 250){
-  testModule1.write1(3, 0x90);
-  testModule1.write1(2, 0x90);
-  testModule1.write1(5, 0x00);
-  }else{
-  testModule1.write1(3, 0x00);
-  testModule1.write1(2, 0x00);
-  testModule1.write1(5, 0x00);
-  }
-  if (light3 < 250){
-  testModule2.write1(3, 0x90);
-  testModule2.write1(2, 0x90);
-  testModule2.write1(5, 0x00);
-  }  else{
-  testModule2.write1(3, 0x00);
-  testModule2.write1(2, 0x00);
-  testModule2.write1(5, 0x00);
-  }
-  if (light4 < 250){
-  testModule3.write1(3, 0x90);
-  testModule3.write1(2, 0x90);
-  testModule3.write1(5, 0x00);
-  } else{
-  testModule3.write1(3, 0x00);
-  testModule3.write1(2, 0x00);
-  testModule3.write1(5, 0x00);
-  }
-  Serial.println("-------------------------------------------------------");
-  Serial.println("Ambient light intensity 1: " + String(light1, 1) + " lx");
-  Serial.println("Ambient light intensity 2: " + String(light2, 1) + " lx");
-  Serial.println("Ambient light intensity 3: " + String(light3, 1) + " lx");
-  Serial.println("Ambient light intensity 4: " + String(light3, 1) + " lx");
-  delay(1000);
-}
 
 bool setBusChannel(uint8_t i2c_channel)
 {
@@ -170,3 +34,198 @@ bool setBusChannel(uint8_t i2c_channel)
     return true;
   }
 }
+
+PCA9634 testModule(0x1C);
+PCA9634 testModule1(0x1C);
+PCA9634 testModule2(0x1C);
+PCA9634 testModule3(0x1C);
+/*
+  I2C порт 0x07 - выводы GP16 (SDA), GP17 (SCL)
+  I2C порт 0x06 - выводы GP4 (SDA), GP13 (SCL)
+  I2C порт 0x05 - выводы GP14 (SDA), GP15 (SCL)
+  I2C порт 0x04 - выводы GP5 (SDA), GP23 (SCL)
+  I2C порт 0x03 - выводы GP18 (SDA), GP19 (SCL)
+*/
+void setup() {
+  // Инициализация последовательного порта
+  Serial.begin(115200);
+
+
+
+
+
+
+
+  Wire.begin();
+  delay(1000);
+
+  // Инициализация датчика #1
+  setBusChannel(0x04); // функция смены I2C-порта
+  testModule.begin();
+  setBusChannel(0x04);
+  LightSensor_1.begin();
+
+  // Инициализация датчика #2
+  setBusChannel(0x05); // функция смены I2C-порта
+  testModule1.begin();
+  setBusChannel(0x05);
+  LightSensor_2.begin();
+
+  // Инициализация датчика #3
+  setBusChannel(0x06); // функция смены I2C-порта
+  testModule2.begin();
+  setBusChannel(0x06);
+  LightSensor_3.begin();
+
+  setBusChannel(0x07); // функция смены I2C-порта
+    testModule3.begin();
+    setBusChannel(0x07);
+  LightSensor_4.begin();
+setBusChannel(0x04);
+    for (int channel = 0; channel < testModule.channelCount(); channel++)
+  {
+    setBusChannel(0x04);
+    testModule.setLedDriverMode(channel, PCA9634_LEDON);
+    Serial.println(channel);
+    delay(500);
+    setBusChannel(0x04);
+    testModule.setLedDriverMode(channel, PCA9634_LEDOFF);
+    delay(500);
+  }
+  setBusChannel(0x04);
+  for (int channel = 0; channel < testModule.channelCount(); channel++)
+  {
+    testModule.setLedDriverMode(channel, PCA9634_LEDPWM); // установка режима ШИМ (0-255)
+  }
+    setBusChannel(0x05);
+    for (int channel = 0; channel < testModule1.channelCount(); channel++)
+  {
+    setBusChannel(0x05);
+    testModule1.setLedDriverMode(channel, PCA9634_LEDON);
+    Serial.println(channel);
+    delay(500);
+    setBusChannel(0x05);
+    testModule1.setLedDriverMode(channel, PCA9634_LEDOFF);
+    delay(500);
+  }
+  setBusChannel(0x05);
+  for (int channel = 0; channel < testModule1.channelCount(); channel++)
+  {
+    setBusChannel(0x05);
+    testModule1.setLedDriverMode(channel, PCA9634_LEDPWM); // установка режима ШИМ (0-255)
+  }
+  setBusChannel(0x06);
+    for (int channel = 0; channel < testModule2.channelCount(); channel++)
+  {
+    setBusChannel(0x06);
+    testModule2.setLedDriverMode(channel, PCA9634_LEDON);
+    Serial.println(channel);
+    delay(500);
+    setBusChannel(0x06);
+    testModule2.setLedDriverMode(channel, PCA9634_LEDOFF);
+    delay(500);
+  }
+  setBusChannel(0x06);
+  for (int channel = 0; channel < testModule2.channelCount(); channel++)
+  {
+    setBusChannel(0x06);
+    testModule2.setLedDriverMode(channel, PCA9634_LEDPWM); // установка режима ШИМ (0-255)
+  }
+  setBusChannel(0x07);
+    for (int channel = 0; channel < testModule3.channelCount(); channel++)
+  {
+    setBusChannel(0x07);
+    testModule3.setLedDriverMode(channel, PCA9634_LEDON);
+    Serial.println(channel);
+    delay(500);
+    setBusChannel(0x07);
+    testModule3.setLedDriverMode(channel, PCA9634_LEDOFF);
+    delay(500);
+  }
+  setBusChannel(0x07);
+  for (int channel = 0; channel < testModule3.channelCount(); channel++)
+  {
+    setBusChannel(0x07);
+    testModule3.setLedDriverMode(channel, PCA9634_LEDPWM); // установка режима ШИМ (0-255)
+  }
+}
+void loop() {
+  // Считывание датчиков
+  setBusChannel(0x04);
+  float light1 = LightSensor_1.readLightLevel();
+  setBusChannel(0x05);
+  float light2 = LightSensor_2.readLightLevel();
+  setBusChannel(0x06);
+  float light3 = LightSensor_3.readLightLevel();
+  setBusChannel(0x07);
+  float light4 = LightSensor_4.readLightLevel();
+
+
+  if (light1 < 250){
+    setBusChannel(0x04);
+  testModule.write1(3, 0x90);
+    setBusChannel(0x04);
+  testModule.write1(2, 0x90);
+    setBusChannel(0x04);
+  testModule.write1(5, 0x00);
+  }else{
+    setBusChannel(0x04);
+  testModule.write1(3, 0x00);
+  setBusChannel(0x04);
+  testModule.write1(2, 0x00);
+  setBusChannel(0x04);
+  testModule.write1(5, 0x00);
+  }
+  if (light2 < 250){
+  setBusChannel(0x05);
+  testModule1.write1(3, 0x90);
+  setBusChannel(0x05);
+  testModule1.write1(2, 0x90);
+  setBusChannel(0x05);
+  testModule1.write1(5, 0x00);
+  }else{
+    setBusChannel(0x05);
+  testModule1.write1(3, 0x00);
+  setBusChannel(0x05);
+  testModule1.write1(2, 0x00);
+  setBusChannel(0x05);
+  testModule1.write1(5, 0x00);
+  }
+  if (light3 < 250){
+    setBusChannel(0x06);
+  testModule2.write1(3, 0x90);
+  setBusChannel(0x06);
+  testModule2.write1(2, 0x90);
+  setBusChannel(0x06);
+  testModule2.write1(5, 0x00);
+  }  else{
+    setBusChannel(0x06);
+  testModule2.write1(3, 0x00);
+  setBusChannel(0x06);
+  testModule2.write1(2, 0x00);
+  setBusChannel(0x06);
+  testModule2.write1(5, 0x00);
+  }
+  if (light4 < 250){
+    setBusChannel(0x07);
+  testModule3.write1(3, 0x90);
+  setBusChannel(0x07);
+  testModule3.write1(2, 0x90);
+  setBusChannel(0x07);
+  testModule3.write1(5, 0x00);
+  } else{
+    setBusChannel(0x07);
+  testModule3.write1(3, 0x00);
+  setBusChannel(0x07);
+  testModule3.write1(2, 0x00);
+  setBusChannel(0x07);
+  testModule3.write1(5, 0x00);
+  }
+  Serial.println("-------------------------------------------------------");
+  Serial.println("Ambient light intensity 1: " + String(light1, 1) + " lx");
+  Serial.println("Ambient light intensity 2: " + String(light2, 1) + " lx");
+  Serial.println("Ambient light intensity 3: " + String(light3, 1) + " lx");
+  Serial.println("Ambient light intensity 4: " + String(light3, 1) + " lx");
+  delay(1000);
+}
+
--- Svetofor/Svetofor.ino
+++ Svetofor/Svetofor.ino
@@ -1,42 +1,115 @@
 #include <FastLED.h>
-
+#include <Wire.h>
 #define LED_PIN     15
-#define LED_PIN2  14
+#define LED_PIN2    14
 #define NUM_LEDS    3
 #define BRIGHTNESS  50
-
+#include <VL53L0X.h>
+VL53L0X lox;
 CRGB leds[NUM_LEDS];
+
+// Переменные для подсчета машин
+int count = 0;
+unsigned long lastResetTime = 0;
+const unsigned long resetInterval = 60000;
+unsigned long lastDetectionTime = 0;
+const unsigned long carCooldown = 0;
+
+// Состояние светофора
+enum TrafficLightState { RED, YELLOW_AFTER_RED, GREEN, YELLOW_AFTER_GREEN };
+TrafficLightState currentState = RED;
+unsigned long stateStartTime;
+
+// Интервалы светофора
+unsigned long redDuration = 15000;
+unsigned long yellowDuration = 2000;   
+unsigned long greenDuration = 15000;  
+unsigned long highTrafficRedDuration = 5000;    
+unsigned long highTrafficGreenDuration = 20000; 
 
 void setup() {
   FastLED.addLeds<NEOPIXEL, LED_PIN>(leds, NUM_LEDS);
   FastLED.addLeds<NEOPIXEL, LED_PIN2>(leds, NUM_LEDS);
   FastLED.setBrightness(BRIGHTNESS);
+  Wire.begin();
+  lox.init();
+  lox.setTimeout(500);
+  lox.setMeasurementTimingBudget(200000);
+  Serial.begin(115200);
+  lastResetTime = millis();
+  stateStartTime = millis();
 }
 
 void loop() {
+  unsigned long currentTime = millis();
 
-  leds[0] = CRGB::Red;
-  leds[1] = CRGB::Black;
-  leds[2] = CRGB::Black; 
+  if (currentTime - lastDetectionTime > carCooldown) {
+    float dist = lox.readRangeSingleMillimeters();
+    if (dist < 60) {
+      count++;
+      lastDetectionTime = currentTime;
+      Serial.print("Машина обнаружена! Всего: ");
+      Serial.println(count);
+
+      delay(100);
+    }
+  }
+
+  if (currentTime - lastResetTime >= resetInterval) {
+    Serial.print("Сброс счетчика. Было машин: ");
+    Serial.println(count);
+    count = 0;
+    lastResetTime = currentTime;
+  }
+
+
+  bool highTraffic = (count >= 4);
+  unsigned long currentRedDuration = highTraffic ? highTrafficRedDuration : redDuration;
+  unsigned long currentGreenDuration = highTraffic ? highTrafficGreenDuration : greenDuration;
+
+  // 4. Управление светофором
+  switch(currentState) {
+    case RED:
+      leds[0] = CRGB::Red;
+      leds[1] = CRGB::Black;
+      leds[2] = CRGB::Black;
+      if (currentTime - stateStartTime >= currentRedDuration) {
+        currentState = YELLOW_AFTER_RED;
+        stateStartTime = currentTime;
+      }
+      break;
+      
+    case YELLOW_AFTER_RED:
+      leds[0] = CRGB::Black;
+      leds[1] = CRGB::Yellow;
+      leds[2] = CRGB::Black;
+      if (currentTime - stateStartTime >= yellowDuration) {
+        currentState = GREEN;
+        stateStartTime = currentTime;
+      }
+      break;
+      
+    case GREEN:
+      leds[0] = CRGB::Black;
+      leds[1] = CRGB::Black;
+      leds[2] = CRGB::Green;
+      if (currentTime - stateStartTime >= currentGreenDuration) {
+        currentState = YELLOW_AFTER_GREEN;
+        stateStartTime = currentTime;
+      }
+      break;
+      
+    case YELLOW_AFTER_GREEN:
+      leds[0] = CRGB::Black;
+      leds[1] = CRGB::Yellow;
+      leds[2] = CRGB::Black;
+      if (currentTime - stateStartTime >= yellowDuration) {
+        currentState = RED;
+        stateStartTime = currentTime;
+      }
+      break;
+  }
+
   FastLED.show();
-  delay(15000); 
-
-  leds[0] = CRGB::Black;
-  leds[1] = CRGB::Yellow;
-  leds[2] = CRGB::Black;
-  FastLED.show();
-  delay(2000);
-
-
-  leds[0] = CRGB::Black;
-  leds[1] = CRGB::Black;
-  leds[2] = CRGB::Green;
-  FastLED.show();
-  delay(15000);
-
-  leds[0] = CRGB::Black;
-  leds[1] = CRGB::Yellow;
-  leds[2] = CRGB::Black;
-  FastLED.show();
-  delay(2000);
+  delay(10);
 }
